2d2212f740e47f1aae29903a3fe783d51b281c10
aro1009 <aro1009@alu.ubu.es>
2022-12-14 17:28:38 +0100

    Create introducirContraseña.py

---
+++ introducirContraseña.py
@@ -0,0 +1,7 @@
+'''
+El usuario deberá introducir la contraseña que desea utilizar para sus inicios de sesión.
+La contraseña deberá tener:
+    - Más de 6 caracteres
+    - Al menos una letra mayúscula
+    - Al menos un número
+'''
