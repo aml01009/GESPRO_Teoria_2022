0bbbc2ca5d2b237976911b7936d21b884a802072
davidA653 <102759478+davidA653@users.noreply.github.com>
2022-12-20 12:32:53 +0100

    tabla de sanciones

---
+++ sancionesUltimoGranPremio.py
@@ -0,0 +1,6 @@
+#TABLA DE SANCIONES DEL ULTIMO GRAN PREMIO
+'''
+En esta tabla van aparecer las sanciones que se ponen a los pilotos. En ella
+aparecerá el nombre del piloto, circuito, la vuelta de la infracción, motivo y
+sanción oportuna.
+'''
