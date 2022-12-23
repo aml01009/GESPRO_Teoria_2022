26c27600987d7959e787d46672ef69bd5183870d
rrf1003 <rrf1003@alu.ubu.es>
2022-12-23 11:51:26 +0100

    Update explicacionDeLaPractica.py

--- explicacionDeLaPractica.py
+++ explicacionDeLaPractica.py
@@ -4,4 +4,7 @@ creamos una rama por cada tarea para hacer el readme de cada historia de usuario
 En esta práctica 2 hemos creado las historias en Jira y hemos creado tareas dentro de cada historia de usuario y hemos creado la rama 
 "Desarrollo", a partir de la cuál hemos creado una rama por cada historia de usuario y en cada rama hemos creado los archivos
 explicando que hace cada tarea.
+
+Si ves que hay cambios raros o que hay muchos cambios o pull request o algo que no te cuadre, ha sido porque nos hemos líado un poco
+a la hora de subir las carpetas y tareas a las diferentes ramas pero creemos que la práctica de control de versiones esta realizada correctamente.
 '''
